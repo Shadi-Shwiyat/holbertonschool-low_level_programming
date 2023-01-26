6ec52d5f5c78d31fc8bc2795142a4c2c02f213fa
Shadi-Shwiyat <raz611103@gmail.com>
2023-01-26 13:07:50 -0800

    C program prints size of different data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,9 +14,9 @@ int main(void)
 	long long int longlongintType;
 	float floatType;
 
-	printf("Size of a char: %d bytes(s)\n", sizeof(charType));
-	printf("Size of an int: %d bytes(s)\n", sizeof(intType));
-	printf("Size of a long int: %d bytes(s)\n", sizeof(longintType));
-	printf("Size of a long long int: %d bytes(s)\n", sizeof(longlongintType));
-	printf("Size of a float: %d bytes(s)\n", sizeof(floatType));
+	printf("Size of a char: %d byte(s)\n", sizeof(charType));
+	printf("Size of an int: %d byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %d byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: %d byte(s)\n", sizeof(floatType));
 }
