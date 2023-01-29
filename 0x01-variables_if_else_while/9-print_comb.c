ebd8a3d2472dff203729801e6620ab86f9227721
Shadi-Shwiyat <raz611103@gmail.com>
2023-01-29 09:58:28 -0800

    Program prints all single digit numbers followed by , and a space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - function prints all combinations
+ * of single digit numbers
+ * 
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int numbers;
+
+	for (numbers = '0'; numbers <= '9'; numbers++)
+	{
+		putchar(numbers);
+
+		if (numbers != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
