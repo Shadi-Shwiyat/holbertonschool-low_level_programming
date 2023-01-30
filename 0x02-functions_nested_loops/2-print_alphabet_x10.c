3afd2feafcd89491424dcd38835cab7616ab2098
Shadi-Shwiyat <raz611103@gmail.com>
2023-01-30 12:27:32 -0800

    Program prints lowercase alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints
+ * alphabet in lower case 10 times
+ *
+ * Return: Void
+ */
+
+void print_alphabet_x10(void)
+{
+	int x= 0;
+	char alpha;
+	
+	while( x <= 9 )
+	{
+		for( alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+		(x++);
+	}
+}
