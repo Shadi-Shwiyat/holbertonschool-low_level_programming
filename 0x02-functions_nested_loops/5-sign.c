b064179599406e015069d2b0a884d9622eba909f
Shadi-Shwiyat <raz611103@gmail.com>
2023-01-30 14:13:49 -0800

    Program determines if input is positive, negative, or zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - function prints the sign
+ * of a number (+ or _ or 0)
+ *
+ * @c: The character to be checked
+ *
+ * Return: 1 for positive, 0 for 0, -1 for negative
+ */
+
+int print_sign(int n)
+{
+	if (n >= 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
