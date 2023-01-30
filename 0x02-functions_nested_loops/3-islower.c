5ce9b2d7299a2fc76419821ce76eb3acbd8e2843
Shadi-Shwiyat <raz611103@gmail.com>
2023-01-30 13:09:28 -0800

    Program checks if input is lower or upper case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * int _islower(int c) - function checks 
+ * _islower - function checks
  * for lower case character
+ *
+ * @c: The character to be checked
  *
  * Return: 1 for lower case and 0 for anything else
  */
 
 int _islower(int c)
 {
-	if (c>= 97 && c<= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
