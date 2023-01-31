a2e3d11af22f0df89d30dc24b86f7420e135c034
Shadi-Shwiyat <raz611103@gmail.com>
2023-01-30 19:11:37 -0800

    Program adds two intergers and returns result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two values
+ *
+ * @n1: first number to add
+ * @n2: second number to add
+ *
+ * Return: Interger value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
