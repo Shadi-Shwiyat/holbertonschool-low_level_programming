2536379fc38fdbf69903c4217565c626ba476579
Shadi-Shwiyat <raz611103@gmail.com>
2023-02-05 14:01:24 -0800

    Program Prints numbers 1 to 100 replacing multiples of 3,5, and 15 with Fizz, Buzz, and FizzBuzz respectively

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * fizz_buzz - function prints numbers
+ * 1 to 100 and replaces multiples of 3
+ * with Fizz, multiples of 5 prints Buzz,
+ * and multiples of 15 prints FizzBuzz.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num1;
+
+	for (num1 = 1; num1 <= 100; num1++)
+	{
+		if ((num1 % 3) == 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if ((num1 % 5) == 0)
+		{
+			printf("%s ", "Buzz");
+		}
+		else if ((num1 % 15) == 0)
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else
+		{
+			printf("%d ", num1);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
