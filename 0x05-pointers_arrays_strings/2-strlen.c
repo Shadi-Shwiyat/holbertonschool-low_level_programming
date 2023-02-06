ad5677063322e3875a75a2e7f84ea8b98019571e
Shadi-Shwiyat <raz611103@gmail.com>
2023-02-06 09:08:19 -0800

    

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - function returns lenghts of string
+ *
+ *@s: string to determine its lenght
+ *
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s);
+{
+	int length;
+
+	while (*s != '\0')
+	{
+		length = length + 1;
+		s = s + 1;
+	}
+	return (length);
+}
