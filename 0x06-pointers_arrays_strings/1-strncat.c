04c59b467a10939d32398875cde0d1dd7df46e3c
Shadi-Shwiyat <raz611103@gmail.com>
2023-02-10 10:34:42 -0800

    Function concatenates two string but only copies the src n number of times

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * *_strcat - function concatenates two different
- * strings
+ * *_strncat - function concatenates two different
+ * strings but stops copying the src at the value of n
  * @dest: the string that has the source added to the end
  * @src: the string that is added to the end of dest
  * @n: the number of characters to stop on src
