7e29745a0a4752a69655d03cc5e58fd96d4a460d
Shadi-Shwiyat <raz611103@gmail.com>
2023-02-12 11:27:59 -0800

    Function capitalizes the first letter of each word in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,7 +4,7 @@
  * *cap_string - function capitalizes all
  *	words of a string
  *
- * @s: the string to capitalize all the 
+ * @s: the string to capitalize all the
  *	first letter of words in
  *
  *
@@ -16,7 +16,8 @@ char *cap_string(char *s)
 	int i = 0;
 	int x;
 	int maxChar = 13;
-	char delim[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+	char delim[] = {
+		32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
 
 	while (s[i] != '\0')
 	{
@@ -24,7 +25,7 @@ char *cap_string(char *s)
 
 		while (x < maxChar)
 		{
-			if ((i == 0 || s[i-1] == delim[x]) && (s[i] >= 97 && s[i] <= 122))
+			if ((i == 0 || s[i - 1] == delim[x]) && (s[i] >= 97 && s[i] <= 122))
 			{
 				s[i] -= 32;
 			}
