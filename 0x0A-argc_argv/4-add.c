8a38ec974a715945936e23355da076c50105952f
Shadi-Shwiyat <raz611103@gmail.com>
2023-02-19 10:43:45 -0800

    Program adds positive numbers and returns error if there is a non positive number found

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - program adds positive numbers
+ * @argc: the number of command line arguements
+ * @argv: an array of strings of argc
+ *
+ *
+ * Return: 0 if no number is passed to the program,
+ * the result if success,
+ * and 1 if the arguments contain symbols that
+ * are not number and prints error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!isdigit(argv[i][0]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += atoi(argv[i]);
+			}
+		}
+
+		printf("%d\n", sum);
+		return (sum);
+	}
+
+	else
+	{
+		printf("0\n");
+		return (0);
+	}
+}
