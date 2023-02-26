67f9e099fe19418453d19e0f93e8d383c05afe38
Shadi-Shwiyat <raz611103@gmail.com>
2023-02-26 11:14:01 -0800

    Created a dynamic library using functions we have previously created

--- 0x0A-dynamic_libraries/0-isupper.c
+++ 0x0A-dynamic_libraries/0-isupper.c
@@ -1,7 +1,19 @@
 #include "main.h"
-
+/**
+* _isupper - checks to see if c is uppercase
+* @c: character checked
+*
+* Return: 1 if uppercase and 0 if otherwise
+*/
 int _isupper(int c)
-{
 
+{
+	int i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (i == c)
+			return (1);
+	}
 	return (0);
 }
