2a242d99604107e2519ae7709b548962ce7b43ff
Shadi-Shwiyat <raz611103@gmail.com>
2023-03-03 12:18:17 -0800

    Function creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,32 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 
 /**
- * print_dog - function prints structure dog
+ * new_dog - function creates a new dog
  *
- * @d: the structure to print
+ * @name: name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
  *
- * Return: void
+ * Return: dog_t a structure with info on
+ * the new dog
  */
 
-void print_dog(struct dog *d)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
+	dog_t *newdog;
+	char *name_cpy, *owner_cpy;
 
-	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
+	if (!name || age < 0 || !owner)
+		return (NULL);
 
-	if (d->age != d->age)
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %f\n", d->age);
+	newdog = malloc(sizeof(*newdog));
+	name_cpy = malloc(strlen(name) + 1);
+	owner_cpy = malloc(strlen(owner) + 1);
 
-	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+	if (!newdog || !name_cpy || !owner_cpy)
+	{
+		free(name_cpy);
+		free(owner_cpy);
+		free(newdog);
+		return (NULL);
+	}
+
+	strcpy(name_cpy, name);
+	strcpy(owner_cpy, owner);
+
+	newdog->name = name_cpy;
+	newdog->age = age;
+	newdog->owner = owner_cpy;
+
+	return (newdog);
 }
