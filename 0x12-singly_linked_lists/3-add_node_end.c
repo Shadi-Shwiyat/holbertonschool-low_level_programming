19b2c0679c96bbabc53ed9025777cbb4182d8ba3
Shadi-Shwiyat <raz611103@gmail.com>
2023-03-16 09:23:47 -0700

    Function adds a node to the end of a linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,34 +1,44 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stddef.h>
 
 /**
- * add_node - function adds a node
- * to a linked list
+ * add_node_end - function adds a node
+ * to the end of a linked list
  * @head: the list to add a node to
  * @str: the string to place in the new node
  * Return: the address of new element, otherwise
  * NULL if it failed
  */
 
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	const list_t *current = h;
-	size_t count = 0;
+	list_t *new_node, *current;
 
-	while (current != NULL)
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
-		if (current->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%u] %s\n", current->len, current->str);
-		}
-		count++;
-		current = current->next;
+		free(new_node);
+		return (NULL);
 	}
 
-	return (count);
+	new_node->len = strlen(new_node->str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	current = *head;
+	while (current->next != NULL)
+		current = current->next;
+
+	current->next = new_node;
+
+	return new_node;
 }
