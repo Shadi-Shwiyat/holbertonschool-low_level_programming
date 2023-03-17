d0e960ab08594351cf93f3a8d8e74e73bb580134
Shadi-Shwiyat <raz611103@gmail.com>
2023-03-17 10:11:21 -0700

    Function frees an int list and sets the head pointer to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function frees a list,
+ * then sets head to NULL
+ *
+ * @head: pointer to the list
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+	*head = NULL;
+}
