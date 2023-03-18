0f9a3b8584d58cf27cb825188ad97aec671e03ed
Shadi-Shwiyat <raz611103@gmail.com>
2023-03-18 12:21:51 -0700

    Function deletes a node at a given index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		prev = *head;
 		*head = (*head)->next;
 		free(prev);
-		return(1);
+		return (1);
 	}
 	current = *head;
 	prev = NULL;
