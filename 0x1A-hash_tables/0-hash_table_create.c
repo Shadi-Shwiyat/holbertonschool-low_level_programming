d9e9d3be3707348a7a95f4ad7d0f8688b3ddcc53
Shadi-Shwiyat <raz611103@gmail.com>
2023-03-26 10:36:03 -0700

    Function creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,13 +17,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
-		return NULL;
+		return (NULL);
 
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 	{
 		free(new_table);
-		return NULL;
+		return (NULL);
 	}
 
 	for (i = 0; i < size; i++)
@@ -31,5 +31,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new_table->size = size;
 
-	return new_table;
+	return (new_table);
 }
