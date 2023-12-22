bc8407781b7b2f8d38770aa8bf8960758d449beb
Shadi-Shwiyat <raz611103@gmail.com>
2023-12-21 23:10:44 -0600

    fixing linter issue task 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -8,20 +8,20 @@
  *
  * Return: The first index where the value is located, or -1 if the value is not present or if the array is NULL.
  */
-int linear_search(int *array, size_t size, int value) {
+int linear_search(int *array, size_t size, int value)
+{
     size_t i;
 
-    if (array == NULL) {
-        return -1;  /* Return -1 if the array is NULL */
-    }
+    if (array == NULL)
+        return (-1);  /* Return -1 if the array is NULL */
 
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < size; i++)
+    {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
-        if (array[i] == value) {
-            return (int)i;  /* Return the index if the value is found */
-        }
+        if (array[i] == value)
+            return ((int)i);  /* Return the index if the value is found */
     }
 
-    return -1;  /* Return -1 if the value is not found in the array */
+    return (-1);  /* Return -1 if the value is not found in the array */
 }
