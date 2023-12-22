e44706914ffb1e62703f2e37c5ed422f4b8fefd7
Shadi-Shwiyat <raz611103@gmail.com>
2023-12-21 22:52:07 -0600

    task 1 fix linter issues

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * linear_search - Search for a value in an array using the Linear search algorithm.
+ * @array: Pointer to the first element of the array to search in.
+ * @size: Number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: The first index where the value is located, or -1 if the value is not present or if the array is NULL.
+ */
 int linear_search(int *array, size_t size, int value) {
     size_t i;
 
